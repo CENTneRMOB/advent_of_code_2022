b51e70bdb29435301291722d9d3e748b575eb7d9
Boris P <6479500dev@gmail.com>
2022-12-17 16:24:05 +0400

    day 6 added

---
+++ day_6/task_1.mjs
@@ -0,0 +1,21 @@
+import readFile from '../inputReader.mjs';
+
+const filepath = 'day_6/input.txt';
+
+const string = readFile(filepath);
+
+const solution = (string) => {
+  let result;
+  for (let i = 3; i < string.length; i += 1) {
+    const substr = string.substring(i - 3, i + 1);
+    const isUniq = new Set(substr).size === substr.length;
+    if (isUniq) {
+      result = i + 1;
+      break;
+    }
+  }
+
+  return result;
+};
+
+console.log(solution(string));
